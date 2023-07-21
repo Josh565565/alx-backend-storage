e2596cb5c58f11f6b691b9e54b042e8036d9f76d
Okechukwu Okwor <joshuaokwor565@icloud.com>
2023-07-21 16:54:27 -0700

    Fixed task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
+"""web module
 """
-create a web cach
-"""
+from functools import wraps
+from typing import Callable
 import redis
 import requests
-rc = redis.Redis()
-count = 0
+
+_redis = redis.Redis()
+_redis.flushdb()
 
 
+def count_requests(method: Callable) -> Callable:
+    """count_requests function
+
+    Args:
+        method (Callable): method
+
+    Returns:
+        Callable: wrapper
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wrapper function
+
+        Returns:
+            [type]: wrapper
+        """
+        url = args[0]
+        cached = _redis.get(f"cached:{url}")
+        if cached:
+            return cached.decode("utf-8")
+        response = method(*args, **kwargs)
+        _redis.incr(f"count:{url}")
+        _redis.setex(f"cached:{url}", 10, response)
+        return response
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+    """get_page function
 
+    Args:
+        url (str): url
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    Returns:
+        str: response
+    """
+    response = requests.get(url, timeout=10)
+    return response.text
